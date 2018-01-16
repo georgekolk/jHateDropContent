cfe11c2bcc92bbd202a5e4f258b2d0b04f1dacfb
George Kolk <2525pala@gmail.com>
2018-01-17 01:30:18 +0200

    add rgb gen

---
+++ src/sample/GenerateRandomColorRGB.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.util.Random;
+
+public class GenerateRandomColorRGB {
+
+    static Random randomGenerator;
+
+    static {
+        randomGenerator = new Random();
+    }
+
+    static String generateColor() {
+        int newColor = 0x1000000 + randomGenerator.nextInt(0x1000000);
+        return "#" + Integer.toHexString(newColor).substring(1, 7);
+    }
+}
